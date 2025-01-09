2626ec6c4f1a31a81d76141e9fabec4e40cea152
lathatik <lavanyereddyt437@gmail.com>
2025-01-09 18:05:38 +0530

    initial commit

---
+++ src/main/java/com/example/HelloJava/HelloJavaApplication.java
@@ -0,0 +1,22 @@
+package com.example.HelloJava;
+
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJavaApplication {
+   public static Logger logger= LoggerFactory.getLogger(HelloJavaApplication.class);
+
+   @PostConstruct
+   public void intt(){
+	   logger.info("Application Started");
+   }
+	public static void main(String[] args) {
+		logger.info("Application Executed");
+		SpringApplication.run(HelloJavaApplication.class, args);
+	}
+
+}
